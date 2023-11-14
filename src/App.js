a78eddaf06ca00b719e2f0fcc37a00c71696f39d
fabalvesfr <fabio9162@gmail.com>
2023-11-14 11:52:17 -0300

    Added useQuery hook to fetch data from an external API

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import { Profile } from "./pages/profile";
 import { Contact } from "./pages/contact";
 import { Navbar } from "./navbar";
 import { createContext, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext();
+
+const client = new QueryClient();
 
 function App() {
   const [userName, setUserName] = useState("Fabio");
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ userName, setUserName }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} /> {/*Home page*/}
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />{" "}
-            {/* path whenever the user types an invalid url */}
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ userName, setUserName }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} /> {/*Home page*/}
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />{" "}
+              {/* path whenever the user types an invalid url */}
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,46 @@
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
   const { userName } = useContext(AppContext);
-  return <h1>This is the home page for {userName}</h1>;
+
+  const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json();
+    return data;
+  };
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: "cat-fact-query",
+    queryFn: getCatFact,
+  });
+
+  if (isError) {
+    return <h1>Sorry, there was an error fetching the data.</h1>;
+  }
+
+  if (isLoading) {
+    return (
+      <h1>
+        Loading...
+        <br /> Wait until fetching is done.
+      </h1>
+    );
+  }
+
+  return (
+    <div>
+      <h1>This is the home page for {userName}</h1>
+      <h2>
+        And this is a random cat fact for you sourced from the{" "}
+        <a href="https://catfact.ninja/fact" target="_blank">
+          Cat Fact API
+        </a>{" "}
+        :
+        <br />
+        {data?.fact}
+      </h2>
+      <button onClick={refetch}>Update Cat Fact</button>
+    </div>
+  );
 };
